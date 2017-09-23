10aaedca16eb7d349a1985e8a694730874816fef
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2017-09-23 06:32:00 -0400

    Remove unused reports-per-* views

--- views/reports-per-android-sdk-version/map.js
+++
@@ -1,5 +0,0 @@
-function(doc) {
-    if(doc.BUILD.VERSION.SDK) {
-        emit(doc.BUILD.VERSION.SDK, 1);
-    }
-}
--- views/reports-per-device/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-
-    // !code vendor/acra-storage/utils.js
-    if(doc.APP_VERSION_NAME) {
-        emit(utils.getDevice(doc), 1);
-    }
-}
